e23668e7806f89d0669a234c1a0e9735ed05138b
Lex <wsad101@yandex.ru>
2022-06-12 16:01:43 +0300

    all files

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let res = {};
+  let discriminant = b ^ 2 - 4 * a * c;
+  console.log('Дискриминант = ' + discriminant);
+  if (discriminant < 0) {   
+  };
+  console.log('Корней нет');
+  if (discriminant == 0) {
+    (-b + Math.sqrt(discriminant)) / (2 * a);
+  };
+  if (discriminant > 0) {  
+    ((-b + Math.sqrt(D)) / (2 * a));
+    ((-b - Math.sqrt(D)) / (2 * a));
+  };
+  return arr; 
+};
+console.log(solveEquation(1, 12, 36));
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
